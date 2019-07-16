bf7ed2234a9dcdab9cd15d715f3f7305111bf393
atuldgcp <atuld.gcp@gmail.com>
2019-07-16 23:25:29 +0530

    Committing with Subnet

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "new-dev-network"{
     name = "newdevlopmentnetwork"
-    auto_create_subnetworks = true
+    auto_create_subnetworks = false
 }
 
 resource "aws_vpc" "env_example_two" {
@@ -9,5 +9,25 @@ resource "aws_vpc" "env_example_two" {
   enable_dns_support = true
   tags {
       Name = "terraform-aws-vpc-example-two"
+  }
+}
+
+resource "aws_subnet" "subnet1"{
+  vpc_id = "${aws_vpc.env_example_two.id}"
+  cidr_block = "${cidrsubnet(aws_vpc.env_example_two.cidr_block, 3, 1)}"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+resource "aws_subnet" "subnet2"{
+  vpc_id = "${aws_vpc.env_example_two.id}"
+  cidr_block = "${cidrsubnet(aws_vpc.env_example_two.cidr_block, 2, 2)}"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "PrivateSubnet2"
   }
 }
